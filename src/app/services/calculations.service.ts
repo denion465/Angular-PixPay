e27ef52afc047b67791137db0f82831956272c1c
Daniel Vidal <danielsv465@hotmail.com>
2021-04-16 20:18:14 -0300

    Adicionando MongoDB e services para comunicação com a api-node

--- src/app/services/calculations.service.ts
+++ src/app/services/calculations.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import  AppSettings from '../AppSettings'
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
